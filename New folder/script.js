38c2bf638c5b45fbfa38bad9f95d4e45ab06ee68
hammadzaher <zaheerhammad62@gmail.com>
2024-09-06 10:59:37 +0500

    hammad ko muzamil ko ye bhejnA HA

---
+++ New folder/script.js
@@ -0,0 +1,8 @@
+let num1 = 8;
+let num2 = 10;
+let num3 = 12;
+let num4 = 28;
+
+let result = --num1 + ++num2 + num3++ + --num4;
+
+console.log("num1:" , num1 , "num2:" ,num2 , "num3:" , num3 ,"num4:" , num4,"result:",result );
